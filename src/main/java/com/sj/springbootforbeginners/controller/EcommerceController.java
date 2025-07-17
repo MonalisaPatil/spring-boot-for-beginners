8e817a39b8998e1a372f425260092580d9f55a73
Monalisa Patil <90000984+MonalisaPatil@users.noreply.github.com>
2025-07-17 15:25:11 -0500

    Added changes specific to data ingestion

--- src/main/java/com/sj/springbootforbeginners/controller/EcommerceController.java
+++ src/main/java/com/sj/springbootforbeginners/controller/EcommerceController.java
@@ -24,48 +24,44 @@ public class EcommerceController {
     ShoppingTrendsRepository shoppingTrendsRepository;
 
 
-    @GetMapping(value = "/getCustomerWrtLessThanGivenAge",produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Integer> getCustomerWrtLessThanGivenAge(int Age){
+    @GetMapping(value = "/getCustomerWrtLessThanGivenAge", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Integer> getCustomerWrtLessThanGivenAge(int Age) {
         return shoppingTrendsService.getCustomerWrtLessThanGivenAge(Age);
     }
 
-    @GetMapping(value ="/getCustomerWrtPurchaseFrequency" , produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Integer>  getCustomerWrtPurchaseFrequency(String Frequency){
+    @GetMapping(value = "/getCustomerWrtPurchaseFrequency", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Integer> getCustomerWrtPurchaseFrequency(String Frequency) {
         return shoppingTrendsService.getCustomerWrtPurchaseFrequency(Frequency);
     }
 
-    @GetMapping(value ="/getUniqueFrequency" , produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<String> getUniqueFrequency(){
-        return  shoppingTrendsService.getUniqueFrequency();
+    @GetMapping(value = "/getUniqueFrequency", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getUniqueFrequency() {
+        return shoppingTrendsService.getUniqueFrequency();
     }
 
-    @GetMapping(value ="/getItemPurchasedWrtReview" , produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<String> getItemPurchasedWrtReview(Double reviewRating){
+    @GetMapping(value = "/getItemPurchasedWrtReview", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getItemPurchasedWrtReview(Double reviewRating) {
         return shoppingTrendsService.getItemPurchasedWrtReview(reviewRating);
     }
 
-    @GetMapping(value ="/getCustomerWrtGivenItemColor" , produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Integer>  getCustomerWrtGivenItemColor(String Color){
+    @GetMapping(value = "/getCustomerWrtGivenItemColor", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Integer> getCustomerWrtGivenItemColor(String Color) {
         return shoppingTrendsService.getCustomerWrtGivenItemColor(Color);
     }
 
 
-
-
-
-
-    @GetMapping(value ="/custom/getAllShoppingTrendData" , produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<ShoppingTrend>  getAllShoppingTrendData(){
+    @GetMapping(value = "/custom/getAllShoppingTrendData", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ShoppingTrend> getAllShoppingTrendData() {
         List<ShoppingTrend> shoppingTrends = shoppingTrendsRepository.findAll();
         return shoppingTrends;
     }
 
-    @GetMapping(value ="/custom/basedOnSeason" , produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<ShoppingTrend>  getCustomerBasedOnSeason(String season){
+    @GetMapping(value = "/custom/basedOnSeason", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ShoppingTrend> getCustomerBasedOnSeason(String season) {
         List<ShoppingTrend> filteredShoppingTrends = new ArrayList<>();
         List<ShoppingTrend> shoppingTrends = shoppingTrendsRepository.findAll();
-        for( ShoppingTrend S :shoppingTrends){
-            if(S.getSeason().equalsIgnoreCase(season)){
+        for (ShoppingTrend S : shoppingTrends) {
+            if (S.getSeason().equalsIgnoreCase(season)) {
                 filteredShoppingTrends.add(S);
             }
         }
@@ -74,24 +70,61 @@ public class EcommerceController {
 
     // TODO: 10/21/23 write a program  to extract all the customers ids whose age is lesser than given input
     @GetMapping(value = "/custom/basedOnAgeRange", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Integer> getCustomerBasedOnAgeRange(int age){
-         List<Integer> customerIdList = new ArrayList<>();
+    public List<Integer> getCustomerBasedOnAgeRange(int age) {
+        List<Integer> customerIdList = new ArrayList<>();
         List<ShoppingTrend> shoppingTrends = shoppingTrendsRepository.findAll();
-        for(ShoppingTrend S : shoppingTrends){
-            if(S.getAge()<age){
+        for (ShoppingTrend S : shoppingTrends) {
+            if (S.getAge() < age) {
                 customerIdList.add(S.getCustomerID());
             }
         }
         return customerIdList;
     }
 
-// TODO: 10/21/23  write a program  to extract all the  customer ids whose purchase frequency is annually::
+    // TODO: 10/21/23  write a program  to extract all the  customer ids whose purchase frequency is annually::
+    @GetMapping(value = "/custom/basedOnPurchaseFrequency", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Integer> getCustomerBasedOnPurchaseFrequency() {
+        List<Integer> annuallyPurchaseCustomerIdList = new ArrayList<>();
+        List<ShoppingTrend> shoppingTrends = shoppingTrendsRepository.findAll();
+        for (ShoppingTrend S : shoppingTrends) {
+            if (S.getFrequencyOfPurchases().equalsIgnoreCase("annually")) {
+                annuallyPurchaseCustomerIdList.add(S.getCustomerID());
+            }
+        }
+        return annuallyPurchaseCustomerIdList;
+    }
 
-// TODO: 10/21/23  write a program  to extract the all the shopped items whose review rating is less than given input::
+    // TODO: 10/21/23  write a program  to extract the all the shopped items whose review rating is less than given input::
+    @GetMapping(value = "/custom/basedOnReviewRatings", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getItemsBasedOnReviewRatings(double rate) {
+        List<String> itemsBasedOnRatingsList = new ArrayList<>();
+        List<ShoppingTrend> shoppingTrends = shoppingTrendsRepository.findAll();
+        for (ShoppingTrend S : shoppingTrends) {
+            if (S.getReviewRating() < rate) {
+                itemsBasedOnRatingsList.add(S.getItemPurchased());
+
+            }
+
+        }
+        return itemsBasedOnRatingsList;
+    }
+}
+
+
 // TODO: 10/21/23  write a program  to extract the all the customer ids that shopped items of given color;
+ @GetMapping(value = "/custom/basedOnColor", produces = MediaType.APPLICATION_JSON_VALUE)
+public 
+
+
+
+
+
+
+
+
+
+
+
 
 // TODO: 10/21/23  write a program  to extract the all the unique frequency of purchases
 
-
-
-}
